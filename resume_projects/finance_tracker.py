af6e74232e4cceebd732767ae4bb5d5913f5f2ae
fabiancella <fabiancella@gmail.com>
2024-10-21 09:42:29 -0400

    More functions for data collection and processing

--- resume_projects/finance_tracker.py
+++ resume_projects/finance_tracker.py
@@ -1,7 +1,14 @@
 print("Welcome to the Personal Finance Tracker program")
 print("\nThis program will help you budget on a monthly basis according to income and transactions that you provide.")
-print("Categories consist of grocries, transportation")
-running = True
+print("Categories consist of groceries, transportation, housing, pets, insurance, etc.")
+mainrunning = True
+subrunning = True
+while True:
+        try:
+            annual_income = int(input("\nPlease enter your annual income: "))
+            break  # Exit the income input loop if the input is valid
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 transactions = []
 # function to store transcation factors
@@ -26,23 +33,70 @@ def data_collection():
         desc = input("Please enter a brief description: ").title()
         print("Adding...")
 
-        # adding to array
+        # adding data to array
         add_transaction(cat, amount, desc)
         print("\nTransaction added")
+
         for i in transactions:
-            print("Category:", i['category'], "\nAmount: $", i['amount'], "\nDescription:", i['description'])
+            print("\nCategory:", i['category'], "\nAmount: $" + str(i['amount']), "\nDescription:", i['description'])
+        return choice()
+
+# def sort_category():
+
 
 def choice():
-    global running
+    global subrunning
     user_choice = input("\nDo you want to add another transaction (yes/no)? ").lower()
-    if user_choice == "yes":
-        return data_collection
-    elif user_choice == "no":
-        print("\nOkay, no more")
-        running = False
-    else:
-        print("Invalid input, please type 'yes' or 'no'.")
-        return choice
+    while True:    
+        if user_choice == "yes":
+            return data_collection()
+        elif user_choice == "no":
+            print("\nOkay, no more")
+            choice2 = input("Would you like to sort the list by category or by amount (cat/num)?: ")
+            print("\nAll transactions")
+            print("Category:\t\tAmount:\t\t\tDescription:")
+            for i in transactions:
+                    if choice2 == "cat":
+                        sorted_transactions = sorted(transactions, key=lambda transaction: transaction['category'][0])
+                        print(i['category'], "\t\t\t", i['amount'], "\t\t", i['description'])
+                    elif choice2 == "num":
+                        sorted_transactions = sorted(transactions, key=lambda transaction: transaction['amount'])
+                        print(i['category'], "\t\t\t", i['amount'], "\t\t", i['description'])
+            subrunning = False
+            break
+        else:
+            print("Invalid input, please type 'yes' or 'no'.")
+            return choice()
+
+# function to calculate the monthly expenses
+def total_expenses():
+    total_amount = 0
+    for transacion in transactions:
+        total_amount += transacion['amount']
+    print("\nTotal monthly expenses: $" + str(total_amount))
+    return total_amount
+# Calculate total expenses and store the result
+
+# initialized list and func for tracking saving goals    
+goals_list = []
+def goals(bills, spending, saving):
+    goal_hash = {
+        "bills": bills,
+        "spending": spending,
+        "saving": saving,
+    }
+    goals_list.append(goal_hash)
+
+# inputs for percentage of income to save, spend, and use for bills 
+def goal_input():
+    print("\nThe standard rule is 50% for bills, 30% for spending, and 20% to save.")
+    bills_input = int(input("Enter what percentage a month you would like to allocate towards bills (e.g., 50 for 50%): ")) / 100
+    spending_input = int(input("Enter what percentage a month you would like to spend (e.g., 30 for 30%): ")) / 100
+    saving_input = int(input("Enter what percentage a month you would like to save (e.g., 20 for 20%): ")) / 100
+    print("\nCalculating...")
+    
+    #adds to array
+    goals(bills_input, spending_input, saving_input)
 
 
 def debt(debt_amount, category, amount):
@@ -51,19 +105,21 @@ def debt(debt_amount, category, amount):
     except ValueError:
        print("Please try again and enter a number")
         
-# user input their income and expenses
-annual_income = int(input("\nPlease enter your annual income: "))
-
 #Loop for program
-while running == True:    
-    #calling data collection function
-    data_collection()
-    choice()
+while mainrunning == True:
+    # user input their expenses
+    while subrunning == True:
+        data_collection()
     
-    # if choice() == "no":
-    #     print("Calculating monthly budget according to your annual income and monthly expenses")
-    #     annual_income /= 12
-    #     print("\nTotal monthly income\n")
-    #     for i in ['amount']:
-    #         print(i)
-            
+    total_expenses_value = total_expenses()
+    # inputs for percentage of income to save, spend, and use for bills
+    goal_input()
+    monthly_income = annual_income/12
+    for i in goals_list:
+        bills_percent = i['bills'] * monthly_income
+        spending_percent = i['spending'] * monthly_income
+        saving_precent = i['saving'] * monthly_income
+    print("You should allocate $" + str(bills_percent) + " to bills, $" + str(spending_percent) + " for spending, and $" + str(saving_precent) + " for saving every month")
+
+    
+    break
